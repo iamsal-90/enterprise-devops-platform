6bc0bb7d32ac3b1c6bfa22c569825eefb91b421f
Salar <saly@git.com>
2026-07-20 11:17:01 +0200

    Add Iam role policy fot aws loadbalancer controller

---
+++ terraform/environments/dev/lbc_iam.tf
@@ -0,0 +1,49 @@
+data "tls_certificate" "eks" {
+  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_policy" "aws_lbc" {
+  name        = "AWSLoadBalancerControllerIAMPolicy-${var.environment}"
+  path        = "/"
+  description = "AWS Load Balancer Controller IAM Policy"
+  policy      = file("${path.module}/iam_policy.json")
+}
+
+resource "aws_iam_role" "aws_lbc" {
+  name = "aws-load-balancer-controller-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Condition = {
+          StringEquals = {
+            "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+            "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "aws_lbc" {
+  policy_arn = aws_iam_policy.aws_lbc.arn
+  role       = aws_iam_role.aws_lbc.name
+}
+
+output "aws_lbc_role_arn" {
+  value       = aws_iam_role.aws_lbc.arn
+  description = "IAM Role ARN for AWS Load Balancer Controller"
+}
